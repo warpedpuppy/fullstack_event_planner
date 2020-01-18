79ccb8bff3d111e8691cd5d69c319d948f254ab3
ted <ted@warpedpuppy.com>
2020-01-18 14:59:39 -0500

    remove data folder

--- frontend/data/maine.js
+++
@@ -1,4 +0,0 @@
-
-export default {
-    towns: ['Bowdoinham','Bremen','Bristol','Brunswick','Camden','Cushing','Damariscotta','Dresden','Edgecomb','Friendship','Georgetown','Harpswell','Jefferson','Lincolnville','Monhegan','Newcastle','Nobleboro','Phippsburg','Richmond','Rockland','Rockport','Searsport','Stockton Springs','Southport','Thomaston','Topsham','Union','Waldoboro','Warren','Westport Island','West Bath','Whitefield','Wiscasset','Woolwich']
-}
